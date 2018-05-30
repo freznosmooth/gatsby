72a4dd1b114fcefa9f2c8ca348d7217cf0a529a3
Mike Francis <mike.francis@weareroam.com>
2018-05-30 13:11:15 +1200

    Added about us text

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -4,6 +4,7 @@ import Link from "gatsby-link";
 export default () => (
   <div>
     <h1>About Us</h1>
+    <p>loremDo veniam minim nisi exercitation quis ea amet pariatur sit eiusmod aute esse. Commodo velit ea laborum id enim tempor tempor nisi id occaecat. Nulla irure consequat fugiat aliquip culpa ipsum elit do est anim laborum sunt sunt anim. Et aliqua non exercitation ut. In voluptate culpa proident aute.</p>
     <Link to="/">Back home</Link>
   </div>
 );
