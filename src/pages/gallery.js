83767b344957bc38d40f19d4a240a67c344e70d0
Mike Francis <mike.francis@weareroam.com>
2018-07-18 17:08:15 +1200

    gallery working

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,92 +1,82 @@
-import React from 'react'
+import React, { Component } from 'react';
 import Gallery from 'react-photo-gallery'
-import Lightbox from 'react-images'
+import Lightbox from 'react-images';
 
-class GalleryPage extends React.Component {
+class GalleryPage extends Component {
   constructor() {
-    super();
+    super()
+
     this.state = { 
       currentImage: 0, 
       lightboxIsOpen: true,
     }
   }
   
+  closeLightbox = () => {
+    this.setState({
+      currentImage: 0,
+      lightboxIsOpen: true,
+    });
+  }
+  
+  gotoPrevious = () => {
+    this.setState({
+      currentImage: this.state.currentImage - 1,
+    });
+  }
+  
+  gotoNext = () => {
+    this.setState({
+      currentImage: this.state.currentImage + 1,
+    });
+  }
+
+  getGalleryImages = (data) => {
+    const images = data.allContentfulGallery.edges[0].node.galleryImage
+
+    const galleryImageSet = images.map(image => {
+      return Object.assign({}, {
+        key: image.id,
+        src: image.file.url,
+        srcSet: image.sizes.srcSet,
+        width: image.file.details.image.width,
+        height: image.file.details.image.height,
+        alt: image.file.fileName,
+      })
+    })
+
+    return galleryImageSet
+  }
+
+  getLightboxImages = () => {
+    const images = data.allContentfulGallery.edges[0].node.galleryImage
+    console.log(images)
+
+    const lightboxImageSet = images.map(image => {
+      return Object.assign({}, {
+        src: image.file.url,
+      })
+    })
+
+    console.log(lightboxImageSet)
+
+    return lightboxImageSet
+  }
+
   render() {
     const { data } = this.props
-
-    this.openLightbox = (event, obj) => {
-      this.setState({
-        currentImage: obj.index,
-        lightboxIsOpen: true,
-      });
-    }
-
-    this.closeLightbox = () => {
-      this.setState({
-        currentImage: 0,
-        lightboxIsOpen: false,
-      });
-    }
-
-    this.gotoPrevious = () => {
-      this.setState({
-        currentImage: this.state.currentImage - 1,
-      });
-    }
-
-    this.gotoNext = () => {
-      this.setState({
-        currentImage: this.state.currentImage + 1,
-      });
-    }
-
-    this.getGalleryImages = () => {
-      console.log(data)
-      const images = data.allContentfulGallery.edges[0].node.galleryImage
-      console.log(images)
-
-      const galleryImageSet = images.map(image => {
-        return Object.assign({}, {
-          key: image.id,
-          src: image.file.url,
-          srcSet: image.sizes.srcSet,
-          width: image.file.details.image.width,
-          height: image.file.details.image.height,
-          alt: image.file.fileName,
-        })
-      })
-
-      console.log(galleryImageSet)
-
-      return galleryImageSet
-    }
-
-    this.getLightboxImages = () => {
-      console.log(data)
-      const images = data.allContentfulGallery.edges[0].node.galleryImage
-      console.log(images)
-
-      const lightboxImageSet = images.map(image => {
-        return Object.assign({}, {
-          src: image.file.url,
-        })
-      })
-
-      console.log(lightboxImageSet)
-
-      return lightboxImageSet
-    }
     
     return (
       <div>
-        <Gallery photos={this.getGalleryImages()} />
-        <Lightbox images={this.getLightboxImages()}
-          onClose={this.closeLightbox}
-          onClickPrev={this.gotoPrevious}
-          onClickNext={this.gotoNext}
+        <Gallery photos={this.getGalleryImages(data)} onClick={(e, obj) => {console.log('clicked', e, obj)}}/>
+        {/* <Lightbox
+          images={[{ src: 'http://www.placehold.it/800x400' }, { src: 'http://www.placehold.it/600x900' }]}
           currentImage={this.state.currentImage}
           isOpen={this.state.lightboxIsOpen}
-        />
+          onClickPrev={this.gotoPrevious}
+          onClickNext={this.gotoNext}
+          onClose={this.closeLightbox}
+        /> */}
       </div>
     )
   }
